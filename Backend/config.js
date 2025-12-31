8b657c0430659f553b1749022a4e5ac49185a651
Abdur Rahman <ansarians581@gmail.com>
2025-12-31 16:59:00 +0500

    added remote url

--- Backend/config.js
+++ Backend/config.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB=async ()=>
 {
     try{
-        await mongoose.connect(process.env.DB_URL)
+        await mongoose.connect("mongodb+srv://murdockmatt961_db_user:VwfEaC54xsBzbfQy@cluster0.v4qmnsc.mongodb.net/myDatabase111?retryWrites=true&w=majority&appName=Cluster0");
         console.log("DB connected ....")
 
     }catch(e)
